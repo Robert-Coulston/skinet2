d9160ec086ef583d54f592c7cf247ab854bc77db
Robert-Coulston <robert.coulston@gmail.com>
2023-05-01 19:06:39 +1000

    add loading service and loading timer

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -8,6 +8,7 @@ import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 import { BreadcrumbModule } from 'xng-breadcrumb';
+import { NgxSpinnerComponent, NgxSpinnerModule } from 'ngx-spinner';
 
 
 
@@ -27,9 +28,10 @@ import { BreadcrumbModule } from 'xng-breadcrumb';
       preventDuplicates: true
     }),
     BreadcrumbModule,
+    NgxSpinnerModule,
   ],
   exports: [
-    NavBarComponent, SectionHeaderComponent
+    NavBarComponent, SectionHeaderComponent, NgxSpinnerComponent
   ]
 })
 export class CoreModule { }
